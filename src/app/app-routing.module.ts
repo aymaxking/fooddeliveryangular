eacf5e1fa5d65b55d45ca699a2f60c20a682ddf6
aymaxking <aymanehamidat@gmail.com>
2021-11-18 23:46:43 -0800

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ListPlaceComponent} from "./components/list-place/list-place.component";
+import {EditPlaceComponent} from "./components/edit-place/edit-place.component";
+import {LoginComponent} from "./components/login/login.component";
+import {PageNotFoundComponent} from "./components/partials/page-not-found/page-not-found.component";
+import {AddPlaceComponent} from "./components/add-place/add-place.component";
+import {AuthGuard} from "./guards/auth.guard";
+import {AfterauthGuard} from "./guards/afterauth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", redirectTo: '/places', pathMatch: 'full'
+  },
+  {
+    path: "places", children: [
+      {
+        path: "", component: ListPlaceComponent
+      },
+      {
+        path: "create", component: AddPlaceComponent
+      },
+      {
+        path: "edit/:id", component: EditPlaceComponent
+      },
+    ],canActivate: [AuthGuard]
+  },
+  {
+    path: "login", component: LoginComponent,canActivate: [AfterauthGuard]
+  },
+  {
+    path: "**", component: PageNotFoundComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddPlaceComponent } from './components/add-place/add-place.component';
+import { EditPlaceComponent } from './components/edit-place/edit-place.component';
+import { ListPlaceComponent } from './components/list-place/list-place.component';
+import { NavbarComponent } from './components/partials/navbar/navbar.component';
+import { PageNotFoundComponent } from './components/partials/page-not-found/page-not-found.component';
+import { LoginComponent } from './components/login/login.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {JwtInterceptor} from "./services/jwt.interceptor";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPlaceComponent,
+    EditPlaceComponent,
+    ListPlaceComponent,
+    NavbarComponent,
+    PageNotFoundComponent,
+    LoginComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: JwtInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
